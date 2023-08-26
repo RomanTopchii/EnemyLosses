5ee12ad24ea8eb7b7510b6ba1acc07b6c4869547
Roman Topchii <roma.topchiy@gmail.com>
2023-08-26 10:06:26 +0300

    move api endpoints to the Constants structure

--- EnemyLosses/Repositories/PersonnelLossesRepositoryImpl.swift
+++ EnemyLosses/Repositories/PersonnelLossesRepositoryImpl.swift
@@ -11,7 +11,7 @@ class PersonnelLossesRepositoryImpl: Repository {
     typealias T = PersonnelLossesDto
     
     func getAll() async throws -> [PersonnelLossesDto] {
-        guard let url = URL(string: "https://raw.githubusercontent.com/MacPaw/2022-Ukraine-Russia-War-Dataset/main/data/russia_losses_personnel.json") else { return [] }
+        guard let url = URL(string: Constants.Endpoints.personnelLosses.rawValue) else { return [] }
             let (data, _) = try await URLSession.shared.data(from: url)
             let result = try JSONDecoder().decode([PersonnelLossesDto].self, from: data)
             return result
--- EnemyLosses/Utilities/Constants.swift
+++ EnemyLosses/Utilities/Constants.swift
@@ -11,4 +11,9 @@ struct Constants {
     enum CellsReuseIdentifier: String {
         case dailyLosses = "DailyLossesTableViewCell"
     }
+    
+    enum Endpoints: String {
+        case equipmentLosses = "https://raw.githubusercontent.com/MacPaw/2022-Ukraine-Russia-War-Dataset/main/data/russia_losses_equipment.json"
+        case personnelLosses = "https://raw.githubusercontent.com/MacPaw/2022-Ukraine-Russia-War-Dataset/main/data/russia_losses_personnel.json"
+    }
 }
